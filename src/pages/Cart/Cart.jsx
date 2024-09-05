74469d95fc0e52935c26c4265724c6a1d3d70d40
t-sivakhon.sri <t-sivakhon.sri@axons.in.th>
2024-09-05 16:48:24 +0700

    add Cart

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,13 +1,49 @@
 import React from 'react'
 import './Cart.css'
+import { StoreContext } from '../../context/StoreContext'
+import { useContext } from 'react'
 
 const Cart = () => {
+
+  const {cartItems,food_list,removeFromCart} = useContext(StoreContext);
+
   return (
-    <div>
-      
+    <div className='cart'>
+      <div className="cart-items">
+        <div className="cart-items-title">
+          <p>Items</p>
+          <p>Title</p>
+          <p>Price</p>
+          <p>Quantity</p>
+          <p>Total</p>
+          <p>Remove</p>
+        </div>
+        <br />
+        <hr />
+        {food_list.map((item,index)=>{
+          if(cartItems[item.id]>0)
+          {
+            return (
+              <div>
+                  
+                <div className="cart-items-title cart-items-item">
+                  <img src={item.image} alt="" />
+                  <p>{item.name}</p>
+                  <p>{item.price}</p>
+                  <p>{cartItems[cartItems.id]}</p>
+                  <p>{item.price*cartItems[item.id]}</p>
+                  <p onClick={()=>removeFromCart(item.id)} className='cross'>x</p>
+                </div>
+              <hr />
+              </div>
+              
+            )
+          }
+
+        })}
+      </div>
     </div>
   )
 }
-import './Cart.css'
 
 export default Cart
